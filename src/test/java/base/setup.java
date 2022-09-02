b195987e76358e96cadba9bd16d7d2c19b277d71
aladnansami <aladnansami21@gmail.com>
2022-09-02 11:04:20 +0600

    Dependencies add

---
+++ src/test/java/base/setup.java
@@ -0,0 +1,10 @@
+package base;
+
+import java.net.MalformedURLException;
+
+public class setup {
+    AndroidDriver driver;
+    public AndroidDriver setUp() throws InterruptedException, MalformedURLException {
+
+    }
+}
