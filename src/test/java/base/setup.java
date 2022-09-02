9a454b730e7c0c84e5ceb9277f44d1fa649ca7cf
aladnansami <aladnansami21@gmail.com>
2022-09-02 11:09:43 +0600

    Set up page complete

--- src/test/java/base/setup.java
+++ src/test/java/base/setup.java
@@ -1,10 +1,30 @@
 package base;
 
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
+
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class setup {
     AndroidDriver driver;
+    public static final String  PACKAGE_ID="com.google.android.calculator";
+
+    @BeforeClass
     public AndroidDriver setUp() throws InterruptedException, MalformedURLException {
 
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("deviceName", "My Device");
+        cap.setCapability("platformName", "Android");
+        cap.setCapability("uuid", "ZY3227PBBN");
+        cap.setCapability("appPackage", "com.google.android.calculator");
+        cap.setCapability("appActivity", "com.android.calculator2.Calculator");
+        cap.setCapability("autoGrantPermissions", true);
+        URL url=new URL("http://127.0.0.1:4723/wd/hub");
+        driver=new AndroidDriver(url,cap);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return driver;
     }
 }
